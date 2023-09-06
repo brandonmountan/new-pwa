a9ebe6397da1af6271ea74dd81e39ba52761843f
Brandon Mountan <brandonmountan1@gmail.com>
2023-09-06 16:32:40 -0600

    commit after tutor

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -40,8 +40,8 @@ module.exports = () => {
               description: 'Write whatever',
               background_color: '#225ca3',
               theme_color: '#225ca3',
-              start_url: './',
-              publicPath: './',
+              start_url: '/',
+              publicPath: '/',
               icons: [
                 {
                   src: path.resolve('src/images/logo.png'),
